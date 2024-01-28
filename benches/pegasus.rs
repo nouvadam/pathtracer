8e35e58721a6213838fc31df35ee43decb34b307
nouvadam <lel@lel.lel>
2024-01-28 15:01:46 +0100

    feat: Add Criterion benchmark

---
+++ benches/pegasus.rs
@@ -0,0 +1,156 @@
+use std::time::Duration;
+
+use criterion::{criterion_group, criterion_main, Criterion};
+use pathtracer::hittables::BvhNode;
+use pathtracer::hittables::HittableList;
+use pathtracer::material::*;
+use pathtracer::primitive::*;
+use pathtracer::texture::ConstantTexture;
+use pathtracer::transform::IntoFlipFace;
+use pathtracer::transform::IntoRotated;
+use pathtracer::transform::IntoTranslated;
+use pathtracer::*;
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let mut hittable = HittableList::new();
+    let mut lights = HittableList::new();
+    let mut materials = MaterialContainer::new();
+
+    let _triangle_material = materials.add(Lambertian::new(Box::new(ConstantTexture {
+        color: V3::new(1.0, 0.3, 0.3),
+    })));
+
+    let glass_material = materials.add(Dielectric::new(1.5));
+
+    let red = materials.add(Material::Lambertian(Lambertian {
+        albedo: Box::new(ConstantTexture {
+            color: V3::new(0.65, 0.05, 0.05),
+        }),
+    }));
+    let white = materials.add(Lambertian::new(Box::new(ConstantTexture {
+        color: V3::new(0.73, 0.73, 0.73),
+    })));
+    let green = materials.add(Material::Lambertian(Lambertian {
+        albedo: Box::new(ConstantTexture {
+            color: V3::new(0.12, 0.45, 0.15),
+        }),
+    }));
+    let light = materials.add(Material::LightSource(LightSource {
+        albedo: Box::new(ConstantTexture {
+            color: V3::new(15.0, 15.0, 15.0),
+        }),
+    }));
+
+    hittable.add(Primitive::YZrect(YZrect {
+        y0: 0.0,
+        y1: 1.7,
+        z0: 0.0,
+        z1: 1.7,
+        k: 1.7,
+        material: green,
+    }));
+
+    hittable.add(Primitive::YZrect(YZrect {
+        y0: 0.0,
+        y1: 1.7,
+        z0: 0.0,
+        z1: 1.7,
+        k: 0.0,
+        material: red,
+    }));
+
+    hittable.add(
+        Primitive::XZrect(XZrect {
+            x0: 0.595,
+            x1: 1.105,
+            z0: 0.595,
+            z1: 1.105,
+            k: 1.69,
+            material: light,
+        })
+        .flip_face(),
+    );
+
+    lights.add(
+        Primitive::XZrect(XZrect {
+            x0: 0.595,
+            x1: 1.105,
+            z0: 0.595,
+            z1: 1.105,
+            k: 1.69,
+            material: light,
+        })
+        .flip_face(),
+    );
+
+    hittable.add(Primitive::XZrect(XZrect {
+        x0: 0.0,
+        x1: 1.7,
+        z0: 0.0,
+        z1: 1.7,
+        k: 0.0,
+        material: white,
+    }));
+
+    hittable.add(Primitive::XZrect(XZrect {
+        x0: 0.0,
+        x1: 1.7,
+        z0: 0.0,
+        z1: 1.7,
+        k: 1.7,
+        material: white,
+    }));
+
+    hittable.add(Primitive::XYrect(XYrect {
+        x0: 0.0,
+        x1: 1.7,
+        y0: 0.0,
+        y1: 1.7,
+        k: 1.7,
+        material: white,
+    }));
+
+    hittable.add(
+        Mesh::new("assets/pegasus.obj", glass_material)
+            .unwrap()
+            //.rotate(V3::new(0.0, 1.0, 0.0), 0.8)
+            .rotate(V3::new(0.0, 1.0, 0.0), 3.1415)
+            .translate(V3::new(0.85, 0.0, 0.85)),
+    );
+
+    let image_config = ImageConfig {
+        nx: 1024,
+        ny: 1024,
+        samples_per_pixel: 16,
+        ray_setting: RaySetting {
+            background_color: V3::new(0.0, 0.0, 0.0),
+            depth: 16,
+        },
+        name: "pegasus",
+    };
+
+    let scene = Scene {
+        camera: Camera::new(
+            V3::new(0.85, 0.85, -1.7), //lookfrom
+            V3::new(0.85, 0.85, 0.0),  //lookat
+            V3::new(0.0, 1.0, 0.0),    //vup
+            45.0,                      //vertical_fov
+            1.0,                       //aspect
+            0.0,                       //aperture
+            5.0,                       //focus_dist
+            0.0,                       //time0
+            1.0,                       //time1
+        ),
+        world: BvhNode::new(&hittable),
+        lights: Some(lights),
+        materials,
+    };
+
+    let mut group = c.benchmark_group("renders");
+    group.warm_up_time(Duration::new(15, 0));
+    group.bench_function("pegasus", |b| b.iter(|| scene.render(image_config)));
+    group.finish();
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
---
+++ benches/spheres.rs
@@ -0,0 +1,81 @@
+use std::time::Duration;
+
+use criterion::{criterion_group, criterion_main, Criterion};
+use pathtracer::hittables::BvhNode;
+use pathtracer::material::*;
+use pathtracer::primitive::*;
+use pathtracer::*;
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let mut hittable = Vec::new();
+    let mut materials = MaterialContainer::new();
+
+    //ground
+    hittable.push(Sphere::new(
+        V3::new(0.0, -100.5, -1.0),
+        100.0,
+        materials.add(Metalic::new(V3::new(0.8, 0.8, 0.0), 1.0)),
+    ));
+
+    //center
+    hittable.push(Sphere::new(
+        V3::new(0.0, 0.0, -1.0),
+        0.5,
+        materials.add(Metalic::new(V3::new(0.1, 0.2, 0.5), 1.0)),
+    ));
+
+    //left
+    hittable.push(Sphere::new(
+        V3::new(-1.0, 0.0, -1.0),
+        0.5,
+        materials.add(Dielectric::new(1.5)),
+    ));
+
+    hittable.push(Sphere::new(
+        V3::new(-1.0, 0.0, -1.0),
+        -0.45,
+        materials.add(Dielectric::new(1.5)),
+    ));
+
+    //right
+    hittable.push(Sphere::new(
+        V3::new(1.0, 0.0, -1.0),
+        0.5,
+        materials.add(Metalic::new(V3::new(0.8, 0.6, 0.2), 0.0)),
+    ));
+
+    let image_config = ImageConfig {
+        nx: 512,
+        ny: 512,
+        samples_per_pixel: 16,
+        ray_setting: RaySetting {
+            background_color: V3::new(0.5, 0.7, 1.0),
+            depth: 32,
+        },
+        name: "glass_sphere",
+    };
+
+    let scene = Scene {
+        camera: Camera::new(
+            V3::new(-2.0, 2.0, 1.0), //lookfrom
+            V3::new(0.0, 0.0, -1.0), //lookat
+            V3::new(0.0, 1.0, 0.0),  //vup
+            20.0,                    //vertical_fov
+            2.0,                     //aspect
+            0.02,                    //aperture
+            3.0,                     //focus_dist
+            0.0,                     //time0
+            1.0,                     //time1
+        ),
+        world: BvhNode::new(&hittables::HittableList { list: hittable }),
+        lights: None,
+        materials,
+    };
+    let mut group = c.benchmark_group("renders");
+    group.warm_up_time(Duration::new(15, 0));
+    group.bench_function("spheres", |b| b.iter(|| scene.render(image_config)));
+    group.finish();
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
