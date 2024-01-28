0501095b61725693622c57bc2a97398d92663f50
nouvadam <lel@lel.lel>
2024-01-28 15:00:36 +0100

    refactor: Add tests for multizip

--- src/misc/multizip.rs
+++ src/misc/multizip.rs
@@ -21,8 +21,67 @@ pub trait IntoMultizip<T>: Sized {
     fn multizip(self) -> Multizip<T>;
 }
 
-impl<I: Iterator<Item = Element>, Element: Iterator> IntoMultizip<Element> for I {
-    fn multizip(self) -> Multizip<Element> {
+impl<I, E> IntoMultizip<E> for I
+where
+    I: Iterator<Item = E>,
+    E: Iterator,
+{
+    fn multizip(self) -> Multizip<E> {
         Multizip(self.collect())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use itertools::assert_equal;
+
+    use super::*;
+
+    #[test]
+    fn test_multizip() {
+        let first_vec = vec![1, 2, 3];
+        let second_vec = vec![4, 5, 6];
+        let third_vec = vec![7, 8, 9];
+
+        let result_vec = vec![first_vec, second_vec, third_vec]
+            .into_iter()
+            .map(|element| element.into_iter())
+            .multizip();
+
+        let correct_vec = vec![[1, 4, 7], [2, 5, 8], [3, 6, 9]];
+
+        assert_equal(result_vec, correct_vec);
+    }
+
+    #[test]
+    fn test_multizip2() {
+        let first_vec = vec![1, 2];
+        let second_vec = vec![4, 5, 6];
+        let third_vec = vec![7, 8, 9, 10];
+
+        let result_vec = vec![first_vec, second_vec, third_vec]
+            .into_iter()
+            .map(|element| element.into_iter())
+            .multizip();
+
+        let correct_vec = vec![[1, 4, 7], [2, 5, 8]];
+
+        assert_equal(result_vec, correct_vec);
+    }
+
+    #[test]
+    fn test_multizip3() {
+        let first_vec = vec![1, 2, 3];
+        let second_vec = vec![4, 5, 6];
+        let third_vec = vec![];
+
+        let result_vec = vec![first_vec, second_vec, third_vec]
+            .into_iter()
+            .map(|element| element.into_iter())
+            .multizip();
+
+        let correct_vec: Vec<[i32; 0]> = vec![];
+
+        assert_equal(result_vec, correct_vec);
     }
 }
